42e2d8c5c8a24de57ab6472bb61a764a81d2cfea
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-27 21:02:55 -0400

    stylin

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const mustacheExpress = require('mustache-express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -21,15 +21,10 @@ app.use(session({
   saveUninitialized: true
 }));
 
-let randomImage = function() {
-  var images = ["http://www.fillmurray.com/200/300", "https://images-na.ssl-images-amazon.com/images/I/41oq%2BCg32iL._SY300_.jpg"]
-  let randomIndex = (Math.random() > 0.5) ? 1 : 0;
-  return images[randomIndex];
-};
 
-let captcha = {
-  'http://www.fillmurray.com/200/300': 'billmurray',
-  'https://images-na.ssl-images-amazon.com/images/I/41oq%2BCg32iL._SY300_.jpg': 'notbillmurray'
+let requirments = {
+  'username': 'brandon',
+  'password': 'pickles'
 }
 
 let auth = function(req, res, next) {
@@ -40,7 +35,6 @@ let auth = function(req, res, next) {
   }
 }
 
-//Redirect to either restricted content or login page, depending on login status.
 app.get('/', function(req, res) {
   if (req.session && req.session.admin) {
     res.redirect('/content');
@@ -50,28 +44,25 @@ app.get('/', function(req, res) {
 });
 
 app.get('/login', function(req, res) {
-  res.render('login', {
-    random: randomImage
-  });
+  res.render('login');
 });
 
 app.post('/login', function(req, res) {
-  let correctAnswer = captcha[req.body.displayedImg];
-  if (req.body.guess === correctAnswer) {
+  if (req.body.username === requirments.username && req.body.password === requirments.password) {
     req.session.admin = true;
-    res.redirect('/content');
+    res.redirect('/');
   }
 });
 
 app.get('/content', auth, function(req, res) {
-  res.render('secret-murray');
+  res.render('content')
 });
 
 app.post('/logout', function(req, res) {
   req.session.destroy();
-  res.render('logout')
+  res.render('logout');
 });
 
 app.listen(3000, function() {
   console.log('Login app listening on port 3000');
-})
+});
